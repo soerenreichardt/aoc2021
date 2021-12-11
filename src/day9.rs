a6a939ddde4c2857728d9562679bd116853ae4ea
Soren <soer3nreichardt@googlemail.com>
2021-12-11 23:06:21 +0100

    Refactor day 9 part 1 solution

--- src/day9.rs
+++ src/day9.rs
@@ -1,4 +1,11 @@
+use std::env::var;
 use std::str::FromStr;
+
+const CENTER: usize = 0;
+const LEFT: usize = 1;
+const RIGHT: usize = 2;
+const TOP: usize = 3;
+const BOTTOM: usize = 4;
 
 struct HeightField {
     values: Vec<Vec<i32>>
@@ -14,20 +21,20 @@ impl FromStr for HeightField {
 }
 
 struct MinKernel {
-    min_values: Vec<i32>
+    min_locations: Vec<(usize, usize, i32)>
 }
 
 impl Default for MinKernel {
     fn default() -> Self {
-        Self { min_values: Vec::new() }
+        Self { min_locations: Vec::new() }
     }
 }
 
 impl Kernel<i32> for MinKernel {
-    fn process_neighborhood(&mut self, neighborhood: [[i32; 3]; 3], x: usize, y: usize) {
-        let center = neighborhood[1][1];
+    fn process_neighborhood(&mut self, neighborhood: [i32; 5], x: usize, y: usize) {
+        let center = neighborhood[CENTER];
 
-        let compare_values = [neighborhood[1][0], neighborhood[0][1], neighborhood[2][1], neighborhood[1][2]];
+        let compare_values = [neighborhood[LEFT], neighborhood[RIGHT], neighborhood[TOP], neighborhood[BOTTOM]];
 
         let mut center_is_minimum = true;
         for value in compare_values {
@@ -39,60 +46,42 @@ impl Kernel<i32> for MinKernel {
         }
 
         if center_is_minimum {
-            println!("x[{}] : y[{}]", x, y);
-            println!("{:?}", neighborhood); 
-            self.min_values.push(center + 1);
+            self.min_locations.push((x, y, center + 1));
         } 
     }
 }
 
 trait Kernel<T> {
-    fn process(&mut self, data: Vec<Vec<i32>>) {
+    fn process(&mut self, data: &Vec<Vec<i32>>) {
         for (y, row) in data.iter().enumerate() {
-            for (x, number) in row.iter().enumerate() {
-                let mut neighborhood = [[-1; 3]; 3];
-
-                neighborhood[1][1] = *number;
-                if x > 0 && y > 0 {
-                    neighborhood[0][0] = data[y-1][x-1];
-                } 
-                if x > 0 {
-                    neighborhood[1][0] = data[y][x-1];
-                } 
-                if y > 0 {
-                    neighborhood[0][1] = data[y-1][x];
-                }
-
-                if x < row.len() - 1 && y < data.len() - 1 {
-                    neighborhood[2][2] = data[y+1][x+1];
-                } 
-                if x < row.len() - 1 {
-                    neighborhood[1][2] = data[y][x+1];
-                } 
-                if y < data.len() - 1 {
-                    neighborhood[2][1] = data[y+1][x];
-                }
-
-                if x > 0 && y < data.len() - 1 {
-                    neighborhood[2][0] = data[y+1][x-1];
-                }
-                if y > 0 && x < row.len() - 1 {
-                    neighborhood[0][2] = data[y-1][x+1];
-                }
-
-                self.process_neighborhood(neighborhood, x, y) 
+            for (x, _) in row.iter().enumerate() {
+                self.process_location(data, row.len(), x, y);
             }
         }
     }
 
-    fn process_neighborhood(&mut self, neighborhood: [[i32; 3]; 3], x: usize, y: usize);
+    fn process_location(&mut self, data: &Vec<Vec<i32>>, row_length: usize, x: usize, y: usize) {
+        let mut neighborhood = [-1; 5];
+
+        neighborhood[CENTER] = data[y][x];
+
+        if x > 0 { neighborhood[LEFT] = data[y][x-1]; }
+        if y > 0 { neighborhood[TOP] = data[y-1][x]; }
+
+        if x < row_length - 1 { neighborhood[RIGHT] = data[y][x+1]; }
+        if y < data.len() - 1 { neighborhood[BOTTOM] = data[y+1][x]; }
+
+        self.process_neighborhood(neighborhood, x, y)
+    }
+
+    fn process_neighborhood(&mut self, neighborhood: [i32; 5], x: usize, y: usize);
 }
 
 pub fn lava_tubes(data: &str) {
     let height_field = HeightField::from_str(data).unwrap();
     let mut min_kernel = MinKernel::default();
-    min_kernel.process(height_field.values);
+    min_kernel.process(&height_field.values);
 
-    let min_values_sum = min_kernel.min_values.iter().sum::<i32>();
+    let min_values_sum = min_kernel.min_locations.iter().map(|(_, _, value)| value).sum::<i32>();
     println!("{}", min_values_sum)
 }
