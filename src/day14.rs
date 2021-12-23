e5f6a02fa5d6a86e04f1b66024e5157af14e1223
Sören Reichardt <soren.reichardt@neotechnology.com>
2021-12-23 14:44:56 +0100

    Solve day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -13,34 +13,38 @@ pub fn extended_polymerization(data: &str) {
         rules.insert((from[0], from[1]), into.nth(0).unwrap());
     });
 
-    // let mut rules_map: HashMap<(char, char), u32> = HashMap::new();
-    // input.windows(2).for_each(|window| {
-    //     if let Some(&matching) = rules.get(&(window[0], window[1])) {
-    //         *rules_map.entry((window[0], window[1])).or_insert(0) -= 1;
+    let mut rules_map: HashMap<(char, char), u64> = HashMap::new();
+    input.windows(2).for_each(|window| {
+        *rules_map.entry((window[0], window[1])).or_insert(0) += 1;
+    });
 
-    //         *rules_map.entry((window[0], matching)).or_insert(0) += 1;
-    //         *rules_map.entry((matching, window[1])).or_insert(0) += 1;
-    //     }
-    // });
-    for _ in 0..10 {
-        let mut new_input: Vec<char> = Vec::new();
-        new_input.push(input[0]);
-        input.windows(2).for_each(|window| {
-            if let Some(matching) = rules.get(&(window[0], window[1])) {
-                new_input.push(*matching);
+    for _ in 0..40 {
+        let mut new_rules_map: HashMap<(char, char), u64> = HashMap::new();
+        rules_map.iter().for_each(|(&(a, b), count)| {
+            if let Some(&matching) = rules.get(&(a, b)) {
+                *new_rules_map.entry((a, matching)).or_insert(0) += count;
+                *new_rules_map.entry((matching, b)).or_insert(0) += count;
+            } else {
+                *new_rules_map.entry((a, b)).or_insert(0) += 1;
             }
-            new_input.push(window[1]);
         });
-        input = new_input;
+        rules_map = new_rules_map
     }
 
-    let mut char_count: HashMap<char, u32> = HashMap::new();
-    input.iter().for_each(|&c| {
-        let counter = char_count.entry(c).or_insert(0);
-        *counter += 1;
+    let mut char_count: HashMap<char, u64> = HashMap::new();
+    rules_map.iter().for_each(|(&(a, b), count)| {
+        let counter = char_count.entry(a).or_insert(0);
+        *counter += count;
+
+        let counter = char_count.entry(b).or_insert(0);
+        *counter += count;
     });
+
+    println!("{:?}", char_count);
 
     let mut counts = char_count.values().collect::<Vec<_>>();
     counts.sort();
-    println!("{}", *counts.last().unwrap() - *counts.first().unwrap());
+    let big = *counts.last().unwrap() / 2 + 1;
+    let small = *counts.first().unwrap() / 2 + 1;
+    println!("{}", big - small + 1);
 }
